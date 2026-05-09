077bd466faf117fe28ff6f1be9b6e48ec485d49b
Vichow <398270996@qq.com>
2026-05-09 14:58:45 +0800

    查询订单（服务端）代码开发

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -5,9 +5,11 @@ import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderStatisticsVO;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,10 +34,20 @@ public class OrderController {
     }
 
 
+    /**
+     * 统计订单数据
+     * @return
+     */
     @GetMapping("/statistics")
     public Result<OrderStatisticsVO> statistics() {
         log.info("统计订单信息");
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
         return Result.success(orderStatisticsVO);
+    }
+
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> details(@PathVariable Long id) {
+        log.info("查询订单详情，订单id为：{}", id);
+        return Result.success(orderService.details(id));
     }
 }
